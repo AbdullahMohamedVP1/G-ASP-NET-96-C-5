34a94793624ca8d0fb7342d4b3783de635975f39
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-07-31 18:30:53 +0300

    Question1

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Assignment4
+﻿using System.Drawing;
+
+namespace Assignment4
 {
+    enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
+    }
+
+    class CBook
+    {
+        private string password = "secret";
+        internal int copiesInStock = 5;
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            #
+            CBook book = new CBook();
+
+            #region Question1
+            // Add a private string password = "secret"; field to a Book class.
+            // Try to print it from Main (outside the class).
+            // What happens, and why?
+
+            //Console.WriteLine(book.password);
+
+            // gives error can't access 'password' because it is private
+            // private members can only be accessed inside the same class
+            #endregion
+
         }
     }
 }
